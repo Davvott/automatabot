4bae2a106b853a645b0920b7bd34c16d7126ac59
Sassafrass <stephen.davey@my.jcu.edu.au>
2019-06-25 15:29:46 +1000

    Replace count_nei() with 4 lines of code

--- pythonautomata_bot/automata_bot.py
+++ pythonautomata_bot/automata_bot.py
@@ -55,16 +55,19 @@ class AutomataBot:
     def next_state(self):
         """Parses next game state, returns a list of tile coordinates that will change for next_state
         Tk Grid takes care of flipping their color"""
-        dead_cells = []
-        live_cells = []
+        offset_coords = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
+
         flip_cells = []
-        alive_cells = self.get_alive_cells()  # returns list of pairs
+        # alive_cells = self.get_alive_cells()  # returns list of pairs
 
         for j in range(self.rows):
             for i in range(self.cols):
                 # Count your blessings
-                alive_nei = self.count_neighbors(x=i, y=j, alive=alive_cells)  # returns int
-                # Does it die?
+                nei = []
+                for x, y in offset_coords:  # unpack tuple
+                    if -1 < j + y < self.rows and -1 < x + i < self.cols:
+                        nei.append(self.cells[j + y][i + x])
+                alive_nei = sum(nei)  # Does it die?
                 if alive_nei not in self.survival and self.cells[j][i] == 1:  # dies
                     flip_cells.append([j, i])
                 # Does it arise?
@@ -96,30 +99,6 @@ class AutomataBot:
                 self.success = True
         except KeyError as error:
             print(error)
-
-    def count_neighbors(self, x, y, alive):
-        """Hey Neighbour, with x,y coords and list of live ones - counts surrounding neighbors
-        Returns: account of alive_neighbors, get it?
-        """
-        # Index values for grid
-        x_bound = self.cols
-        y_bound = self.rows
-        alive_neighbors = 0
-
-        # Set Horizontal, Vertical, RDIAG, LDIAG directions
-        for delta_x, delta_y in [(1, 0), (0, 1), (1, 1), (-1, 1)]:
-            # Set delta for +- directions
-            for delta in (1, -1):
-                delta_x *= delta  # (1*1); (1+-1); (0*1); (0*-1) ...
-                delta_y *= delta  # (0*1); (0*-1); (1*1); (1*-1) ...
-                next_x = x + delta_x  # = 1; -1; 0; 0; ...
-                next_y = y + delta_y  # = 0, 0; 1; -1; ...
-
-                if 0 <= next_y <= y_bound and 0 <= next_x <= x_bound:
-                    if [next_y, next_x] in alive:
-                        alive_neighbors += 1
-
-        return alive_neighbors
 
     def get_alive_cells(self):
         """Returns list of pairs of [[y, x],...]"""
